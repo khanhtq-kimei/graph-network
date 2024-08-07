b3c5a11f80b9b6b96b862280f0353ed10fa68ec7
khainp <khainp@kimei.vn>
2024-08-07 09:31:45 +0700

    update: hihi

--- src/dataKWS.js
+++ src/dataKWS.js
@@ -1,46 +1,11 @@
-export const data ={
+export const data = {
     "status": "success",
     "data": {
         "nodes": [
             {
                 "uuid": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5",
                 "title": "Công ty TNHH Một Thành Viên",
-                "nodeType": "root",
-                "goalKind": null,
-                "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
-            },
-            {
-                "uuid": "bbc374a1-785b-4294-87dd-5fd501af871d",
-                "title": "Mục tiêu chiến lược 2",
-                "nodeType": "so",
-                "goalKind": "company",
-                "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
-            },
-            {
-                "uuid": "5738b07b-c010-4700-80ca-5a541fc73d08",
-                "title": "Mục tiêu chiến lược 3",
-                "nodeType": "so",
-                "goalKind": "company",
-                "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
-            },
-            {
-                "uuid": "59ba8bf5-8c78-42be-9934-93ddcd1db470",
-                "title": "123",
-                "nodeType": "o",
-                "goalKind": "detail",
-                "progress": 0,
-                "startDate": "2024-03-31T17:00:00.000Z",
-                "confidenceLevel": null,
-                "targetDate": "2024-06-30T16:59:59.000Z"
+                "nodeType": "root"
             },
             {
                 "uuid": "b8488bb0-952b-499c-863f-2f68f245ff7b",
@@ -48,109 +13,23 @@ export const data ={
                 "nodeType": "so",
                 "goalKind": "company",
                 "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
+                "hasCollapseNode": true
             },
             {
-                "uuid": "bf4ddcd2-0bd5-4e57-9848-e9362d257f5f",
-                "title": "Xe hợp đồng chạy quá tốc độ cả nghìn lần/tháng, vì sao khó xử lý?\n 1213213",
-                "nodeType": "o",
-                "goalKind": "department",
-                "progress": 20,
-                "startDate": "2024-06-30T17:00:00.000Z",
-                "confidenceLevel": 0,
-                "targetDate": "2024-09-30T16:59:59.000Z"
-            },
-            {
-                "uuid": "40123693-35ec-4c3a-afcf-fd4c227ba6ef",
-                "title": "Xe hợp đồng chạy quá tốc độ cả nghìn lần/tháng, vì sao khó xử lý?\n 999",
-                "nodeType": "o",
-                "goalKind": "department",
-                "progress": 20,
-                "startDate": "2024-06-30T17:00:00.000Z",
-                "confidenceLevel": 0,
-                "targetDate": "2024-09-30T16:59:59.000Z"
-            },
-            {
-                "uuid": "7b7a9e7e-70eb-48a0-b2cc-a27e33e91b4f",
-                "title": "Xe hợp đồng chạy quá tốc độ cả nghìn lần/tháng, vì sao khó xử lý? quá nhỉ",
-                "nodeType": "o",
-                "goalKind": "department",
-                "progress": 20,
-                "startDate": "2024-06-30T17:00:00.000Z",
-                "confidenceLevel": 0,
-                "targetDate": "2024-09-30T16:59:59.000Z"
-            },
-            {
-                "uuid": "240e80cd-1b3a-4a52-9e9d-ae281908fd0a",
-                "title": "Mục tiêu của năm 2024 của bạn đã đạt tới đỉnh cao như thế nào trong xã hội cuộc sống xã hội",
-                "nodeType": "o",
-                "goalKind": "company",
-                "progress": 20,
-                "startDate": "2024-06-30T17:00:00.000Z",
-                "confidenceLevel": 0,
-                "targetDate": "2024-09-30T16:59:59.000Z"
-            },
-            {
-                "uuid": "0ed88a21-2e2b-4a3a-903c-75b1880557e4",
-                "title": "Mục tiêu bạn muốn đề xuất thực hiện là gì? mới nhất nè",
-                "nodeType": "o",
-                "goalKind": "company",
-                "progress": 75,
-                "startDate": "2024-03-31T17:00:00.000Z",
-                "confidenceLevel": 5,
-                "targetDate": "2024-06-30T16:59:59.000Z"
-            },
-            {
-                "uuid": "c850f733-b3dd-44b7-bf83-14abf89df84f",
-                "title": "mục tiêu j ta",
+                "uuid": "5738b07b-c010-4700-80ca-5a541fc73d08",
+                "title": "Mục tiêu chiến lược 3",
                 "nodeType": "so",
                 "goalKind": "company",
                 "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
+                "hasCollapseNode": true
             },
             {
-                "uuid": "b975cb96-1b31-4e89-aa05-eefbf7ad8e63",
-                "title": "21312313",
+                "uuid": "bbc374a1-785b-4294-87dd-5fd501af871d",
+                "title": "Mục tiêu chiến lược 2",
                 "nodeType": "so",
                 "goalKind": "company",
                 "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
-            },
-            {
-                "uuid": "27519b10-4b5e-4ad6-878a-20c077adaba8",
-                "title": "21312313",
-                "nodeType": "so",
-                "goalKind": "company",
-                "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
-            },
-            {
-                "uuid": "b784d2a2-2321-41ae-a7a1-194f03af8b36",
-                "title": "21312313",
-                "nodeType": "so",
-                "goalKind": "company",
-                "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
-            },
-            {
-                "uuid": "68766503-5371-4cc8-b45e-656b418e7fbf",
-                "title": "21312313",
-                "nodeType": "so",
-                "goalKind": "company",
-                "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
+                "hasCollapseNode": true
             },
             {
                 "uuid": "2a607640-9fc6-46b9-b880-e698e1c0712d",
@@ -158,19 +37,15 @@ export const data ={
                 "nodeType": "so",
                 "goalKind": "company",
                 "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
+                "hasCollapseNode": true
             },
             {
-                "uuid": "8b341711-bbe2-47e8-948c-d95851ab379d",
-                "title": "21312313",
+                "uuid": "c850f733-b3dd-44b7-bf83-14abf89df84f",
+                "title": "mục tiêu j ta",
                 "nodeType": "so",
                 "goalKind": "company",
                 "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
+                "hasCollapseNode": true
             },
             {
                 "uuid": "597850f3-2e3c-427e-b412-de5dd66d9067",
@@ -178,15 +53,53 @@ export const data ={
                 "nodeType": "so",
                 "goalKind": "company",
                 "progress": null,
-                "startDate": null,
-                "confidenceLevel": null,
-                "targetDate": null
+                "hasCollapseNode": true
+            },
+            {
+                "uuid": "68766503-5371-4cc8-b45e-656b418e7fbf",
+                "title": "21312313",
+                "nodeType": "so",
+                "goalKind": "company",
+                "progress": null,
+                "hasCollapseNode": true
+            },
+            {
+                "uuid": "27519b10-4b5e-4ad6-878a-20c077adaba8",
+                "title": "21312313",
+                "nodeType": "so",
+                "goalKind": "company",
+                "progress": null,
+                "hasCollapseNode": true
+            },
+            {
+                "uuid": "b784d2a2-2321-41ae-a7a1-194f03af8b36",
+                "title": "21312313",
+                "nodeType": "so",
+                "goalKind": "company",
+                "progress": null,
+                "hasCollapseNode": false
+            },
+            {
+                "uuid": "8b341711-bbe2-47e8-948c-d95851ab379d",
+                "title": "21312313",
+                "nodeType": "so",
+                "goalKind": "company",
+                "progress": null,
+                "hasCollapseNode": false
+            },
+            {
+                "uuid": "b975cb96-1b31-4e89-aa05-eefbf7ad8e63",
+                "title": "21312313",
+                "nodeType": "so",
+                "goalKind": "company",
+                "progress": null,
+                "hasCollapseNode": false
             }
         ],
         "links": [
             {
-                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-bbc374a1-785b-4294-87dd-5fd501af871d",
-                "source": "bbc374a1-785b-4294-87dd-5fd501af871d",
+                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-b8488bb0-952b-499c-863f-2f68f245ff7b",
+                "source": "b8488bb0-952b-499c-863f-2f68f245ff7b",
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
@@ -195,39 +108,14 @@ export const data ={
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
-                "id": "5738b07b-c010-4700-80ca-5a541fc73d08-59ba8bf5-8c78-42be-9934-93ddcd1db470",
-                "source": "59ba8bf5-8c78-42be-9934-93ddcd1db470",
-                "target": "5738b07b-c010-4700-80ca-5a541fc73d08"
-            },
-            {
-                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-b8488bb0-952b-499c-863f-2f68f245ff7b",
-                "source": "b8488bb0-952b-499c-863f-2f68f245ff7b",
+                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-bbc374a1-785b-4294-87dd-5fd501af871d",
+                "source": "bbc374a1-785b-4294-87dd-5fd501af871d",
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
-                "id": "b8488bb0-952b-499c-863f-2f68f245ff7b-bf4ddcd2-0bd5-4e57-9848-e9362d257f5f",
-                "source": "bf4ddcd2-0bd5-4e57-9848-e9362d257f5f",
-                "target": "b8488bb0-952b-499c-863f-2f68f245ff7b"
-            },
-            {
-                "id": "b8488bb0-952b-499c-863f-2f68f245ff7b-40123693-35ec-4c3a-afcf-fd4c227ba6ef",
-                "source": "40123693-35ec-4c3a-afcf-fd4c227ba6ef",
-                "target": "b8488bb0-952b-499c-863f-2f68f245ff7b"
-            },
-            {
-                "id": "b8488bb0-952b-499c-863f-2f68f245ff7b-7b7a9e7e-70eb-48a0-b2cc-a27e33e91b4f",
-                "source": "7b7a9e7e-70eb-48a0-b2cc-a27e33e91b4f",
-                "target": "b8488bb0-952b-499c-863f-2f68f245ff7b"
-            },
-            {
-                "id": "b8488bb0-952b-499c-863f-2f68f245ff7b-240e80cd-1b3a-4a52-9e9d-ae281908fd0a",
-                "source": "240e80cd-1b3a-4a52-9e9d-ae281908fd0a",
-                "target": "b8488bb0-952b-499c-863f-2f68f245ff7b"
-            },
-            {
-                "id": "b8488bb0-952b-499c-863f-2f68f245ff7b-0ed88a21-2e2b-4a3a-903c-75b1880557e4",
-                "source": "0ed88a21-2e2b-4a3a-903c-75b1880557e4",
-                "target": "b8488bb0-952b-499c-863f-2f68f245ff7b"
+                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-2a607640-9fc6-46b9-b880-e698e1c0712d",
+                "source": "2a607640-9fc6-46b9-b880-e698e1c0712d",
+                "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
                 "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-c850f733-b3dd-44b7-bf83-14abf89df84f",
@@ -235,8 +123,13 @@ export const data ={
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
-                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-b975cb96-1b31-4e89-aa05-eefbf7ad8e63",
-                "source": "b975cb96-1b31-4e89-aa05-eefbf7ad8e63",
+                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-597850f3-2e3c-427e-b412-de5dd66d9067",
+                "source": "597850f3-2e3c-427e-b412-de5dd66d9067",
+                "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
+            },
+            {
+                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-68766503-5371-4cc8-b45e-656b418e7fbf",
+                "source": "68766503-5371-4cc8-b45e-656b418e7fbf",
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
@@ -250,23 +143,13 @@ export const data ={
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
-                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-68766503-5371-4cc8-b45e-656b418e7fbf",
-                "source": "68766503-5371-4cc8-b45e-656b418e7fbf",
-                "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
-            },
-            {
-                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-2a607640-9fc6-46b9-b880-e698e1c0712d",
-                "source": "2a607640-9fc6-46b9-b880-e698e1c0712d",
-                "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
-            },
-            {
                 "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-8b341711-bbe2-47e8-948c-d95851ab379d",
                 "source": "8b341711-bbe2-47e8-948c-d95851ab379d",
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             },
             {
-                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-597850f3-2e3c-427e-b412-de5dd66d9067",
-                "source": "597850f3-2e3c-427e-b412-de5dd66d9067",
+                "id": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5-b975cb96-1b31-4e89-aa05-eefbf7ad8e63",
+                "source": "b975cb96-1b31-4e89-aa05-eefbf7ad8e63",
                 "target": "e52c6a5d-28e4-4682-86eb-e9b9ad2d30d5"
             }
         ]
